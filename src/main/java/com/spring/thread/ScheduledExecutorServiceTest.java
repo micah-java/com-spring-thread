8d1e72e36d8f1e2535f454b68c4233d23e1b9a23
huo1362120194 <huo1362120194@163.com>
2021-02-05 21:16:18 +0800

    333

--- src/main/java/com/spring/thread/ScheduledExecutorServiceTest.java
+++ src/main/java/com/spring/thread/ScheduledExecutorServiceTest.java
@@ -9,17 +9,18 @@ import java.util.concurrent.TimeUnit;
 
 public class ScheduledExecutorServiceTest {
 
-    public static void main(String[] args) throws Exception {
+    static {
         ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
-
         final Apple apple = new Apple();
         apple.setPrice(123.1);
         // 延时 1 秒后，按 3 秒的周期执行任务
         timer.scheduleAtFixedRate(()->{
             Double price = apple.getPrice();
-            System.out.println("任务开始当前时间:" + new Date());
-            System.out.println("price:" +price);
+            System.out.println("任务开始当前时间:" + new Date()+" price:" + price);
         }, 1000, 3000, TimeUnit.MILLISECONDS);
-        return;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("===================");
     }
 }
