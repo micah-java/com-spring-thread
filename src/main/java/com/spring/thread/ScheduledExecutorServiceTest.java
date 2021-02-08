91b48c1b25aa7d08b022b0bcea965c8aacedf13a
huo1362120194 <huo1362120194@163.com>
2021-02-08 12:09:02 +0800

    4444

--- src/main/java/com/spring/thread/ScheduledExecutorServiceTest.java
+++ src/main/java/com/spring/thread/ScheduledExecutorServiceTest.java
@@ -1,16 +1,22 @@
 package com.spring.thread;
 
 import com.spring.thread.model.Apple;
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 
 import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ScheduledExecutorServiceTest {
 
     static {
-        ScheduledExecutorService timer = Executors.newSingleThreadScheduledExecutor();
+        ScheduledExecutorService timer = null;
+       // timer = Executors.newSingleThreadScheduledExecutor();
+        timer =  new ScheduledThreadPoolExecutor(1,
+                new BasicThreadFactory.
+                        Builder().namingPattern("schedule-pool-%d").daemon(false).build());
         final Apple apple = new Apple();
         apple.setPrice(123.1);
         // 延时 1 秒后，按 3 秒的周期执行任务
